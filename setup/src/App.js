ff434f69ea4015decb790b9cc8c9e8ccff5f57f1
scoreben <96953045+scoreben@users.noreply.github.com>
2022-04-01 02:56:12 +0300

    first commit

---
+++ setup/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+function App() {
+  const [questions, setQuestions] = useState(data);
+ 
+  return (
+    <main>
+      
+      <h3> questions and answers about login</h3>
+      <section className='info'>
+        { 
+          questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+            
+
+          })
+
+
+
+
+        }
+        
+
+     </section>
+    
+    
+    </main>
+  
+  );
+}
+
+export default App;
